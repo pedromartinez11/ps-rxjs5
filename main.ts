a6ddc3407cfed9110166460b1673b2b3879bd268
Pedro M <pedromartinez11@gmail.com>
2017-06-06 22:44:48 -0700

    Remove observer.error() call

--- main.ts
+++ main.ts
@@ -7,9 +7,9 @@ let numbers = [1, 5, 10];
 
 let source = Observable.create((observer) => {
     numbers.forEach((num) => {
-        if (num === 5) {
-            observer.error(`oh no! the num was ${num}`);
-        }
+        // if (num === 5) {
+        //     observer.error(`oh no! the num was ${num}`);
+        // }
 
         observer.next(num);
     });
