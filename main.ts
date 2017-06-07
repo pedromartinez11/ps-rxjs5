f71a96436cc251e3355ad87af0baa4294524af3c
Pedro M <pedromartinez11@gmail.com>
2017-06-06 23:22:29 -0700

    Listen to mouse events and adjust the position of a circle based on the MouseMove event's clientX and clientY after doing a transformation and filtering

--- main.ts
+++ main.ts
@@ -1,38 +1,27 @@
 import { Observable } from 'rxjs';
 
-let numbers = [1, 5, 10];
+let circle = document.getElementById('circle');
 
 /** 'observer' param has the .next(val), error(err), complete() methods */
-let source = Observable.create((observer) => {
-    let i = 0;
-
-    const produceValue = () => {
-        const curVal = numbers[i];
-
-        observer.next(curVal);
-        i++;
-        if (i < numbers.length) {
-            // there' more values to process
-            setTimeout(produceValue, 300);
-        } else {
-            // we're done
-            observer.complete();
+let source = Observable.fromEvent(document, 'mousemove')
+    .map((ev: MouseEvent) => {
+        return {
+            x: ev.clientX,
+            y: ev.clientY
         }
-    }
+    })
+    .filter(val => val.x < 500 && val.y < 500)
+    .delay(300);
 
-    produceValue();
-}).map(n => {
-    // double each val
-    return n * 2;
-}).filter(n => {
-    // only keep items greater than 4
-    return n > 4;
-});;
+const onNext = (value) => {
+    circle.style.left = value.x + 'px';
+    circle.style.top = value.y + 'px';
+};
 
 // simple subscribe that just takes in 3 functions: next(value), error(err), complete();
 // it automatically creates an Observer!
 source.subscribe(
-    (value) => console.log(`value: ${value}`),
+    onNext,
     (err) => console.log(`error: ${err}`),
     () => console.log('complete')
 );
