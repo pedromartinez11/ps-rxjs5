6bfdc0e99861fe785c667f11d8e117b6c8d8b824
Pedro M <pedromartinez11@gmail.com>
2017-06-06 22:49:09 -0700

    Make observable source async by using setTimeout

--- main.ts
+++ main.ts
@@ -4,16 +4,28 @@ let numbers = [1, 5, 10];
 
 
 /** 'observer' param has the .next(val), error(err), complete() methods */
-
 let source = Observable.create((observer) => {
-    numbers.forEach((num) => {
-        observer.next(num);
-    });
+    let i = 0;
 
-    observer.complete();
+    const produceValue = () => {
+        const curVal = numbers[i];
+
+        observer.next(curVal);
+        i++;
+        if (i < numbers.length) {
+            // there' more values to process
+            setTimeout(produceValue, 2000);
+        } else {
+            // we're done
+            observer.complete();
+        }
+    }
+
+    produceValue();
 });
 
 // simple subscribe that just takes in 3 functions: next(value), error(err), complete();
+// it automatically creates an Observer!
 source.subscribe(
     (value) => console.log(`value: ${value}`),
     (err) => console.log(`error: ${err}`),
