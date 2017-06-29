447abea411c91c8e850dcee65c6f5b0f862ef5f2
Pedro Martinez <pedro.martinez@veeva.com>
2017-06-29 15:20:19 -0700

    Use retryWhen and delay for error handling

--- main.ts
+++ main.ts
@@ -5,20 +5,37 @@ let button = document.getElementById('button');
 
 let click = Observable.fromEvent(button, 'click');
 
+const retryStrategy = ({attemps = 3, delay = 1000}) => {
+    return (errors) => {
+        return errors
+            .scan((acc, theErr) => {
+                console.log(`cur acc: ${acc}, val: ${theErr}`);
+                return acc + 1;
+            }, 0)
+            .takeWhile((acc) => {
+                return acc < attemps;
+            })
+            .delay(delay);
+    };
+};
+
 const load = (url: string) => {
-    console.log('load() outer() ');
     return Observable.create(observer => {
         const xhr = new XMLHttpRequest();
 
         xhr.addEventListener('load', () => {
-            const data = JSON.parse(xhr.responseText);
-            observer.next(data);
-            observer.complete();
+            if (xhr.status === 200) {
+                const data = JSON.parse(xhr.responseText);
+                observer.next(data);
+                observer.complete();
+            } else {
+                observer.error(xhr.statusText);
+            }
         });
 
         xhr.open("GET", url);
         xhr.send();
-    })
+    }).retryWhen(retryStrategy({attemps: 5, delay: 2000}));
 };
 
 const renderMoviesFromData = movies => {
@@ -30,7 +47,7 @@ const renderMoviesFromData = movies => {
 }
 
 click
-    .flatMap(ev => load("movies.json"))
+    .flatMap(ev => load("moviess.json"))
     .subscribe(
         data => renderMoviesFromData(data),
         e => console.log('error: ', e),
